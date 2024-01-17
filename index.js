125e4609477ba6f2dbbaa74a2a9dc434f2238718
Sudarkodi <sudarkodimuthiah22@gmail.com>
2024-01-17 03:46:10 -0800

    removed index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-// javascript code to fetch lambda 
-
-const counter = document.querySelector(".counter-number");
-async function updateCounter() {
-    let response = await fetch("https://eyhi6cx3fnwdkr7v3mgcci6kfu0hyqfe.lambda-url.us-west-1.on.aws/");
-    let data = await response.json();
-    counter.innerHTML = `Views: ${data}`;
-}
